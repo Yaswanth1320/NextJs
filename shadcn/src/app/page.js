0da7c44e26e71cfcb94fd33be0b909a89a46c6cf
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2023-12-18 15:21:23 +0530

    motion

--- shadcn/src/app/page.js
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button"
-
-export default function Home() {
-  return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <Button variant='new'>Click here</Button>
-    </div>
-  );
-}
